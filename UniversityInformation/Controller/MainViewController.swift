089a759dca4b1d5e81c40ec0d26c8f2e7e3b980b
akaraoluk <abdurrahmankaraoluk@gmail.com>
2024-04-20 10:19:27 +0300

    [University-Informations][Version 1][Abdurrahman KARAOLUK][-]

---
+++ UniversityInformation/Controller/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  UniversityInformation
+//
+//  Created by Abdurrahman Karaoluk on 19.04.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    //Mark: - Properties
+    //Mark: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
+//Mark: - Helpers
